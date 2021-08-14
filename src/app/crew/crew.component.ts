0f34b801009ad7ea4a10a0a10968ed6b0c52f6c6
Steller.E <steller.e@gmail.com>
2021-08-14 11:33:32 -0500

    done - bonus

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -12,6 +12,20 @@ export class CrewComponent implements OnInit {
     {name: "Mae Jemison", firstMission: false},
     {name: "Ellen Ochoa", firstMission: true}
   ];
+  memberBeingEdited: object = null;
+
+  add(memberName: string, isFirst: boolean) {
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+ }
 
   constructor() { }
 
